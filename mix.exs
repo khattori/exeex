bc216a959ba04dd9f3e0bbd7fc4c81f90e43cbde
Kenta HATTORI <kenta_hattori@hotmail.com>
2020-02-24 08:35:16 +0900

    Elixir version up to 1.9

--- mix.exs
+++ mix.exs
@@ -5,9 +5,10 @@ defmodule ExEEx.MixProject do
     [
       app: :exeex,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
@@ -23,6 +24,7 @@ defmodule ExEEx.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:excoveralls, "~> 0.12.2", only: :test}
     ]
   end
 end
