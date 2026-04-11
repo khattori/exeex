3bfbe0aab6308147569d40227131ea9c83a89e39
Kenta HATTORI <kenta_hattori@hotmail.com>
2026-04-11 13:48:54 +0900

    Updated mix.exs

--- mix.exs
+++ mix.exs
@@ -1,18 +1,18 @@
 defmodule ExEEx.MixProject do
   use Mix.Project
-
+  @description """
+  Exeex is an Elixir template engine that provides macro, include and inheritance functions.
+  It allows you to create reusable templates with dynamic content,
+  making it easier to manage and maintain your views in Elixir applications.
+  """
   def project do
     [
       app: :exeex,
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      description: "Elixir template engine with macro, include and inheritance functions",
-      package: [
-        maintainers: ["Kenta Hattori"],
-        licenses: ["MIT"],
-        links: %{"GitHub" => "https://github.com/khattori/exeex"}
-      ],
+      description: @description,
+      package: package(),
       deps: deps(),
       test_coverage: [tool: ExCoveralls]
     ]
@@ -25,13 +25,21 @@ defmodule ExEEx.MixProject do
     ]
   end
 
+  defp package do
+    [
+      maintainers: ["Kenta Hattori"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/khattori/exeex"}
+    ]
+  end
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:ex_doc, "~> 0.37.3", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.18.5", only: :test}
+      {:ex_doc, "~> 0.40.1", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.18.5", only: :test},
+      {:mix_version, "~> 2.4.0", only: [:dev, :test], runtime: false},
     ]
   end
 end
